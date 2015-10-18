823480bb5310b24305a4d05a359c5def02b2be2f
David Denton <denton.david@gmail.com>
2015-10-18 21:17:21 +0100

    adding other custom randomizers for j8 types

--- src/main/scala/io/github/databob/Randomizers.scala
+++ src/main/scala/io/github/databob/Randomizers.scala
@@ -1,13 +1,26 @@
 package io.github.databob
 
+import java.time._
+
 import org.json4s._
 
+object Randomizers {
+  private val defaultRandomizers = List[Randomizer[_]](
+    new CustomRandomizer[LocalDate](r => LocalDate.of(2000, 1, 1)),
+    new CustomRandomizer[LocalTime](r => LocalTime.of(0, 0, 0)),
+    new CustomRandomizer[LocalDateTime](r => {
+      LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(0, 0, 0))
+    }),
+    new CustomRandomizer[ZonedDateTime](r => {
+      ZonedDateTime.of(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(0, 0, 0)), ZoneId.systemDefault())
+    })
+  )
+}
 case class Randomizers(randomizers: List[Randomizer[_]] = Nil) {
-
-  def +(newSerializer: Randomizer[_]): Randomizers = copy(randomizers = newSerializer :: randomizers)
+  def +(newRandomizer: Randomizer[_]): Randomizers = copy(randomizers = newRandomizer :: randomizers)
 
   def randomizer(format: Randomizers) =
-    randomizers.foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
+    (randomizers ++ Randomizers.defaultRandomizers).foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
       acc.orElse(x.newRandom(format))
     }
 }
@@ -16,9 +29,11 @@ trait Randomizer[A] {
   def newRandom(implicit format: Randomizers): PartialFunction[TypeInfo, A]
 }
 
-class CustomRandomizer[A: Manifest](ser: Randomizers => A) extends Randomizer[A] {
+class CustomRandomizer[A: Manifest](r: Randomizers => A) extends Randomizer[A] {
 
   val Class = implicitly[Manifest[A]].runtimeClass
 
-  def newRandom(implicit format: Randomizers) = { case TypeInfo(Class, _) => ser(format) }
+  def newRandom(implicit format: Randomizers) = {
+    case TypeInfo(Class, _) => r(format)
+  }
 }
