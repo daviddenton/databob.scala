aa1ea717a72c7c71a00c35eff592edd381309dc0
David Denton <denton.david@gmail.com>
2015-10-18 21:23:27 +0100

    renamed custom randomizer to randomiser

--- src/test/scala/io/github/daviddenton/databob/Test.scala
+++ src/test/scala/io/github/daviddenton/databob/Test.scala
@@ -1,10 +1,10 @@
 package io.github.daviddenton.databob
 
-import java.time.{ZonedDateTime, LocalDate}
+import java.time.{LocalDate, ZonedDateTime}
 import java.util.Date
 
 import io.github.databob.Databob.random
-import io.github.databob.Randomizers
+import io.github.databob.{Randomizer, Randomizers}
 
 case class YetAnother(name: Int, which: Boolean, time: Date)
 case class Other(name: String, yet: YetAnother)
@@ -12,6 +12,6 @@ case class Other(name: String, yet: YetAnother)
 case class Person(other: Other, age: Option[ZonedDateTime], bob: LocalDate, names: Seq[Other], aMap: Map[String, ZonedDateTime])
 
 object Test extends App {
-  implicit val f = Randomizers()
+  implicit val f = Randomizers() + Randomizer(r => LocalDate.of(2010, 1, 1))
   println(random[Person])
 }
