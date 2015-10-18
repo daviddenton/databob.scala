f3e2e0048acde2affbd63f8d49e38d2fa3ca93b4
David Denton <denton.david@gmail.com>
2015-10-18 21:44:42 +0100

    tidy

--- src/test/scala/io/github/daviddenton/databob/Test.scala
+++ src/test/scala/io/github/daviddenton/databob/Test.scala
@@ -6,11 +6,13 @@ import io.github.databob.Databob.random
 import io.github.databob.{Randomizer, Randomizers}
 
 case class YetAnother(name: Int, which: Boolean, time: LocalDateTime)
+
 case class Other(name: String, yet: YetAnother)
 
 case class Person(other: Other, age: Option[ZonedDateTime], bob: LocalDate, names: Seq[Other], aMap: Map[String, ZonedDateTime])
 
 object Test extends App {
-  implicit val f = Randomizers() + Randomizer(r => LocalTime.of(10, 1, 1))
+  // fix this so it always passes through the constant set of randomizers (ie. they are locked into the databob instance˚ as a val)
+  implicit val f = Randomizers() + Randomizer(databob => LocalDateTime.of(databob.random[LocalDate], LocalTime.of(12, 12, 12))) + Randomizer(r => LocalDate.of(2010, 12, 1))
   println(random[Person])
 }
