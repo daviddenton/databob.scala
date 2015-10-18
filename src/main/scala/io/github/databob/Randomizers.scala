ec83e2e31a18fc403200f346cab5a7870d3b8e38
David Denton <denton.david@gmail.com>
2015-10-18 21:38:12 +0100

    tidy

--- src/main/scala/io/github/databob/Randomizers.scala
+++ src/main/scala/io/github/databob/Randomizers.scala
@@ -6,23 +6,19 @@ import org.json4s._
 
 object Randomizers {
   private val defaultRandomizers = List[Randomizer[_]](
-    Randomizer(r => LocalDate.of(2000, 1, 1)),
-    Randomizer(r => LocalTime.of(0, 0, 0)),
-    Randomizer(r => {
-      LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(0, 0, 0))
-    }),
-    Randomizer(r => {
-      ZonedDateTime.of(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(0, 0, 0)), ZoneId.systemDefault())
-    })
+    Randomizer(databob => LocalDate.of(2000, 1, 1)),
+    Randomizer(databob => LocalTime.of(0, 0, 0)),
+    Randomizer(databob => LocalDateTime.of(databob.random[LocalDate], databob.random[LocalTime])),
+    Randomizer(databob => ZonedDateTime.of(databob.random[LocalDateTime], ZoneId.systemDefault()))
   )
 }
 
 case class Randomizers(randomizers: List[Randomizer[_]] = Nil) {
   def +(newRandomizer: Randomizer[_]): Randomizers = copy(randomizers = newRandomizer :: randomizers)
 
-  def randomizer(format: Randomizers) =
+  def randomizer(databob: Databob) =
     (randomizers ++ Randomizers.defaultRandomizers).foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
-      acc.orElse(x.newRandom(format))
+      acc.orElse(x.newRandom(databob))
     }
 }
 
