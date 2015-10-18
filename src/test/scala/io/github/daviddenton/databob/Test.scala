faf0adc1c0722784e709d09282d4e73008acad14
David Denton <denton.david@gmail.com>
2015-10-18 21:41:37 +0100

    tidy

--- src/test/scala/io/github/daviddenton/databob/Test.scala
+++ src/test/scala/io/github/daviddenton/databob/Test.scala
@@ -1,12 +1,11 @@
 package io.github.daviddenton.databob
 
-import java.time.{LocalTime, LocalDate, ZonedDateTime}
-import java.util.Date
+import java.time.{LocalDate, LocalDateTime, LocalTime, ZonedDateTime}
 
 import io.github.databob.Databob.random
 import io.github.databob.{Randomizer, Randomizers}
 
-case class YetAnother(name: Int, which: Boolean, time: Date)
+case class YetAnother(name: Int, which: Boolean, time: LocalDateTime)
 case class Other(name: String, yet: YetAnother)
 
 case class Person(other: Other, age: Option[ZonedDateTime], bob: LocalDate, names: Seq[Other], aMap: Map[String, ZonedDateTime])
