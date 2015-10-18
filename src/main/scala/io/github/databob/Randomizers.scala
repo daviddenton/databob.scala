73c111849308206fafdf4734c9e887a7a08842f9
David Denton <denton.david@gmail.com>
2015-10-18 21:41:37 +0100

    tidy

--- src/main/scala/io/github/databob/Randomizers.scala
+++ src/main/scala/io/github/databob/Randomizers.scala
@@ -4,20 +4,20 @@ import java.time._
 
 import org.json4s._
 
-object Randomizers {
+case class Randomizers(randomizers: List[Randomizer[_]] = Nil) {
+
+  implicit val RD = this
   private val defaultRandomizers = List[Randomizer[_]](
     Randomizer(databob => LocalDate.of(2000, 1, 1)),
     Randomizer(databob => LocalTime.of(0, 0, 0)),
     Randomizer(databob => LocalDateTime.of(databob.random[LocalDate], databob.random[LocalTime])),
     Randomizer(databob => ZonedDateTime.of(databob.random[LocalDateTime], ZoneId.systemDefault()))
   )
-}
 
-case class Randomizers(randomizers: List[Randomizer[_]] = Nil) {
   def +(newRandomizer: Randomizer[_]): Randomizers = copy(randomizers = newRandomizer :: randomizers)
 
   def randomizer(databob: Databob) =
-    (randomizers ++ Randomizers.defaultRandomizers).foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
+    (randomizers ++ defaultRandomizers).foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
       acc.orElse(x.newRandom(databob))
     }
 }
