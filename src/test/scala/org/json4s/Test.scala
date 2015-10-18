f7b281f97a511c56a7ed96c04216d474e8957ce8
David Denton <denton.david@gmail.com>
2015-10-18 20:15:20 +0100

    started modelling on json4s example

--- src/test/scala/org/json4s/Test.scala
+++ src/test/scala/org/json4s/Test.scala
@@ -6,6 +6,6 @@ case class Other(name: String, yet: YetAnother)
 case class Person(other: Other, age: Int, bob: String, names: Seq[Other], aMap: Map[String, String])
 
 object Test extends App {
-  implicit val f = DefaultRandomFormats
+  implicit val f = RandomFormats()
   println(Random.random[Person](JObject()))
 }
