4fbb3202ea27385f478ccc22690685b7daedf6e2
David Denton <denton.david@gmail.com>
2015-10-19 17:42:49 +0100

    daveD - added existentials

--- src/main/scala/io/github/databob/RandomType.scala
+++ src/main/scala/io/github/databob/RandomType.scala
@@ -1,5 +1,6 @@
 package io.github.databob
 
 import org.json4s.reflect.{ScalaType, TypeInfo}
+import scala.language.existentials
 
 case class RandomType(ti: TypeInfo, erasure: Class[_], typeArgs: Seq[ScalaType])
