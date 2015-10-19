c3978d5ab3f896ebb4e02b944a26b7b1db8a7038
David Denton <denton.david@gmail.com>
2015-10-19 14:39:21 +0100

    refactoring randomizers

--- src/main/scala/io/github/databob/Randomizers.scala
+++ src/main/scala/io/github/databob/Randomizers.scala
@@ -4,24 +4,30 @@ import java.time._
 
 import org.json4s._
 
-case class Randomizers(randomizers: List[Randomizer[_]] = Nil) {
+case class Randomizers(randomizers: List[Randomizer[_]] = DefaultRandomizers.randomizers) {
 
   implicit val RD = this
-  private val defaultRandomizers = List[Randomizer[_]](
+
+  def +(newRandomizer: Randomizer[_]): Randomizers = copy(randomizers = newRandomizer :: randomizers)
+
+  def ++(that: Randomizers): Randomizers = copy(randomizers = that.randomizers ++ randomizers)
+
+  def randomizer(databob: Databob) =
+    randomizers.foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
+      acc.orElse(x.newRandom(databob))
+    }
+}
+
+object DefaultRandomizers extends Randomizers(
+  Java8DateTimeRandomizers.randomizers
+)
+
+object Java8DateTimeRandomizers extends Randomizers(
+  List(
     Randomizer(databob => LocalDate.of(2000, 1, 1)),
     Randomizer(databob => LocalTime.of(0, 0, 0)),
     Randomizer(databob => LocalDateTime.of(databob.random[LocalDate], databob.random[LocalTime])),
     Randomizer(databob => ZonedDateTime.of(databob.random[LocalDateTime], ZoneId.systemDefault()))
   )
-
-  def +(newRandomizer: Randomizer[_]): Randomizers = copy(randomizers = newRandomizer :: randomizers)
-
-  def randomizer(databob: Databob) =
-    (randomizers ++ defaultRandomizers).foldLeft(Map(): PartialFunction[TypeInfo, Any]) { (acc, x) =>
-      acc.orElse(x.newRandom(databob))
-    }
-}
-
-
-
+)
 
