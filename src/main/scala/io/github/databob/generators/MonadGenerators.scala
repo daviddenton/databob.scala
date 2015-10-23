64d66eb314cda30db7fe1a447af6bebdce0c0ed4
David Denton <denton.david@gmail.com>
2015-10-23 15:50:31 +0100

    daveD - renames of all the Generators to enable other instances such as random

---
+++ src/main/scala/io/github/databob/generators/MonadGenerators.scala
@@ -0,0 +1,28 @@
+package io.github.databob.generators
+
+import io.github.databob.{Databob, Generator, GeneratorFailure, Generators}
+
+import scala.util.control.Exception._
+
+object MonadGenerators {
+  val Default = new Generators(
+    List(
+      new Generator[Option[_]]() {
+        override def mk(databob: Databob) = {
+          case generatorType if classOf[Option[_]].isAssignableFrom(generatorType.erasure) => Option(databob.mk(generatorType.typeArgs.head))
+        }
+      },
+      new Generator[Either[_, _]]() {
+        override def mk(databob: Databob) = {
+          case generatorType if classOf[Either[_, _]].isAssignableFrom(generatorType.erasure) => {
+            (allCatch opt {
+              Left(databob.mk(generatorType.typeArgs.head))
+            } orElse (allCatch opt {
+              Right(databob.mk(generatorType.typeArgs(1)))
+            })).getOrElse(throw new GeneratorFailure("Expected value but got none"))
+          }
+        }
+      }
+    )
+  )
+}
