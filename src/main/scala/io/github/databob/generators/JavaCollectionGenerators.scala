dcbe99aab4535f052090fc6213365286196003b0
David Denton <denton.david@gmail.com>
2015-10-24 17:29:01 +0100

    daveD - renames

--- src/main/scala/io/github/databob/generators/JavaCollectionGenerators.scala
+++ src/main/scala/io/github/databob/generators/JavaCollectionGenerators.scala
@@ -11,13 +11,35 @@ object JavaCollectionGenerators {
    * Generates Empty Java collections
    */
   val Empty =
-    new ErasureMatchingGenerator[Any](_.isArray, (gt, databob) => java.lang.reflect.Array.newInstance(gt.typeArgs.head.erasure, 0)) +
+    new ErasureMatchingGenerator[Any](_.isArray, (gt, databob) => Array()) +
       erasureIs[java.util.List[_]]((databob) => new java.util.ArrayList[Any]()) +
       erasureIs[java.util.Set[_]]((databob) => new java.util.HashSet[Any]()) +
       erasureIs[java.util.Map[_, _]]((databob) => new java.util.HashMap[Any, Any]())
 
   /**
+   * Generates Non-Empty Java collections
+   */
+  val NonEmpty = new ErasureMatchingGenerator[Any](_.isArray, (gt, databob) => {
+    Array(databob.mk(gt.typeArgs.head))
+  }) +
+    erasureIs2[java.util.List[_]]((gt, databob) => {
+      val l = new java.util.ArrayList[Any]
+      l.add(databob.mk(gt.typeArgs.head))
+      l
+    }) +
+    erasureIs2[java.util.Set[_]]((gt, databob) => {
+      val s = new java.util.HashSet[Any]
+      s.add(databob.mk(gt.typeArgs.head))
+      s
+    }) +
+    erasureIs2[java.util.Map[_, _]]((gt, databob) => {
+      val s = new java.util.HashMap[Any, Any]()
+      s.put(databob.mk(gt.typeArgs.head), databob.mk(gt.typeArgs(1)))
+      s
+    })
+
+  /**
    * Generates Random Java collections
    */
-  val Random = Empty
+  val Random = NonEmpty
 }
