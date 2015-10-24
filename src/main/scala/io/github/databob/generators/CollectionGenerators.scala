ca3e8fde6ba032df0b3f14585e08c8390c0bae9a
David Denton <denton.david@gmail.com>
2015-10-24 16:17:58 +0100

    daveD - simplify erase generators

--- src/main/scala/io/github/databob/generators/CollectionGenerators.scala
+++ src/main/scala/io/github/databob/generators/CollectionGenerators.scala
@@ -6,7 +6,6 @@ object CollectionGenerators {
   val Empty = new Generators(
     List(
       new ErasureBasedGenerator[java.util.ArrayList[_]](_.isArray, (gt, databob) => new java.util.ArrayList[Any]()),
-      erasureIsAssignableFrom[java.util.ArrayList[_]]((gt, databob) => new java.util.ArrayList[Any]()),
       erasureIsAssignableFrom[Map[_, _]]((gt, databob) => Map()),
       erasureIsAssignableFrom[Set[_]]((gt, databob) => Set()),
       erasureIsAssignableFrom[List[_]]((gt, databob) => List()),
--- src/test/scala/databob/examples/Examples.scala
+++ src/test/scala/databob/examples/Examples.scala
@@ -4,12 +4,13 @@ import java.time.ZonedDateTime
 
 import io.github.databob.Databob
 
+import scala.util.Try
+
 case class ReadReceipt(readDate: ZonedDateTime)
 case class EmailAddress(value: String)
-case class Email(from: EmailAddress, date: ZonedDateTime, read: Boolean, subject: String, readReceipt: Option[ReadReceipt])
+case class Email(from: EmailAddress, date: ZonedDateTime, read: Boolean, subject: String, readReceipt: Try[ReadReceipt])
 case class Inbox(address: EmailAddress, emails:Seq[Email])
 
 object Examples extends App {
-
   println(Databob.random[Email])
 }
