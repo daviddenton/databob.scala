5ea38637fd746cc7c4a3f94604a0671fe156be26
David Denton <denton.david@gmail.com>
2015-10-25 11:42:13 +0000

    daveD - scaladoc

--- src/test/scala/databob/examples/Examples.scala
+++ src/test/scala/databob/examples/Examples.scala
@@ -3,14 +3,49 @@ package databob.examples
 import java.time.ZonedDateTime
 
 import io.github.databob.Databob
+import io.github.databob.Generator.typeIs
+import io.github.databob.generators.CollectionSize
 
+import scala.collection.immutable.Stream.Empty
 import scala.util.Try
 
 case class ReadReceipt(readDate: ZonedDateTime)
-case class EmailAddress(value: String)
-case class Email(from: EmailAddress, date: ZonedDateTime, read: Boolean, subject: String, readReceipt: Try[ReadReceipt])
-case class Inbox(address: EmailAddress, emails:Seq[Email])
 
+case class EmailAddress(value: String)
+
+case class Email(from: EmailAddress, to: Seq[EmailAddress], date: ZonedDateTime, read: Boolean, subject: String, readReceipt: Try[ReadReceipt])
+
+case class Inbox(address: EmailAddress, emails: Seq[Email])
+
+/**
+ * This set of examples shows how you can use databob to generate objects
+ */
 object Examples extends App {
-  println(Databob.random[Email])
+
+  def completelyRandomObject = Databob.random[Email]
+
+  println(completelyRandomObject)
+
+  def completelyDefaultObject = Databob.default[Email]
+
+  println(completelyDefaultObject)
+
+  def randomObjectWithOverridenField = Databob.random[Email].copy(subject = "my stupid subject")
+
+  println(randomObjectWithOverridenField)
+
+  def objectWithCustomCollectionSizes = {
+    implicit val generators = typeIs(databob => CollectionSize(2)) +
+      typeIs(databob => EmailAddress(databob.mk[String] + "@" + databob.mk[String] + ".com"))
+    Databob.random[Email]
+  }
+
+  println(objectWithCustomCollectionSizes)
+
+  def usingACustomGenerator = {
+    implicit val generators = typeIs(databob => EmailAddress(databob.mk[String] + "@" + databob.mk[String] + ".com")) +: Empty
+    Databob.random[Email]
+  }
+
+  println(usingACustomGenerator)
 }
